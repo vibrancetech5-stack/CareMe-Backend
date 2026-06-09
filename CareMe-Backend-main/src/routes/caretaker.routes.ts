3971d7f990bc1365b5f2afdfce99b39e7a6cce89
vibrancetech5-stack <vibrancetech5@gmail.com>
2026-06-09 11:37:12 +0530

    Delete CareMe-Backend-main directory

--- CareMe-Backend-main/src/routes/caretaker.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { CaretakerController } from '../controllers/caretaker.controller.js';
-
-const router = Router();
-const caretakerController = new CaretakerController();
-
-router.post('/caretakers', caretakerController.createCaretaker.bind(caretakerController));
-
-export default router;
